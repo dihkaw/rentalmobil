ff4847d603f7496a31f3b1b643b69f9a44ade9e0
Wakhid Herri Nurcahyo <162821190+dihkaw@users.noreply.github.com>
2025-01-09 07:54:17 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npm init -y
-npm install express mysql mysql2 dotenv multer ejs body-parser
+npm install express mysql mysql2 mysqldump dotenv multer ejs body-parser aws-sdk 
 npm install -g pm2
 echo "Setup selesai!"
